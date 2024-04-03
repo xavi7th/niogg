6ec2a273a31ce1eba386df8cc17f313fc2ccc7f9
xavi7th <xavi7th@gmail.com>
2024-04-03 08:19:20 +0100

    Install nwidart modules package and publish assets

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,28 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import collectModuleAssetsPaths from './vite-module-loader.js';
 
-export default defineConfig( {
-  plugins: [
-    laravel( {
-      input: 'resources/js/app.js',
-      // refresh: true,
-    } ),
-    svelte( {} )
-  ],
-  resolve: {
-    alias: {
-      '@': '/resources/js',
+async function getConfig () {
+  const paths = [
+    'resources/js/app.js',
+  ];
+  const allPaths = await collectModuleAssetsPaths( paths, 'Modules' );
+
+  return defineConfig( {
+    plugins: [
+      laravel( {
+        input: allPaths,
+        // refresh: true,
+      } ),
+      svelte( {} )
+    ],
+    resolve: {
+      alias: {
+        '@': '/resources/js',
+      },
     },
-  }
-} );
+  } );
+}
+
+export default getConfig();
