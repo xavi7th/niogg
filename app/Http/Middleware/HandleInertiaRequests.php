82381268e7c0d8a86c605b21f3556e1e5f668a3e
xavi7th <xavi7th@gmail.com>
2024-04-04 00:30:18 +0100

    Update HandleInertia Middleware to use module rootview

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,38 +2,43 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Http\Request;
 use Inertia\Middleware;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
 
 class HandleInertiaRequests extends Middleware
 {
-    /**
-     * The root template that is loaded on the first page visit.
-     *
-     * @var string
-     */
-    protected $rootView = 'app';
+  protected $rootView = 'app';
 
-    /**
-     * Determine the current asset version.
-     */
-    public function version(Request $request): string|null
-    {
-        return parent::version($request);
+  /**
+   * Determine the current asset version.
+   */
+  public function version(Request $request): string|null
+  {
+    return parent::version($request);
+  }
+
+  /**
+   * Define the props that are shared by default.
+   *
+   * @return array<string, mixed>
+   */
+  public function share(Request $request): array
+  {
+    return [
+      ...parent::share($request),
+      'auth' => [
+        'user' => $request->user(),
+      ],
+    ];
+  }
+
+  public function rootView(Request $request): string
+  {
+    if (is_null(\Route::currentRouteName())) {
+      return $this->rootView;
     }
 
-    /**
-     * Define the props that are shared by default.
-     *
-     * @return array<string, mixed>
-     */
-    public function share(Request $request): array
-    {
-        return [
-            ...parent::share($request),
-            'auth' => [
-                'user' => $request->user(),
-            ],
-        ];
-    }
+    return Str::before(\Route::currentRouteName(), '.') . '::app';
+  }
 }
