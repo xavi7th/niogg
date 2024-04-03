0f3d90c070a2d1ba98dd7c3a1f147f1d38d15c65
xavi7th <xavi7th@gmail.com>
2024-04-04 00:31:34 +0100

    Update app.js to load module svelte page

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,19 +2,33 @@ import './bootstrap';
 import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/svelte'
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-  resolve: ( name ) => resolvePageComponent( `./Pages/${name}.svelte`, import.meta.glob( './Pages/**/*.svelte' ) ),
-  // resolve: name => {
-  //   const pages = import.meta.glob('./Pages/**/*.svelte', { eager: true })
-  //   return pages[`./Pages/${name}.svelte`]
-  // },
+  resolve: name => {
+    const pages = import.meta.glob(['./Pages/**/*.svelte', '../../Modules/**/Pages/**/*.svelte'], { eager: true })
+
+    let pageUrl = `./Pages/${name}.svelte`;
+
+    if (name.includes('::')) {
+      let [module, pageLocation] = name.split('::');
+      pageUrl = `../../Modules/${module}/resources/js/Pages/${pageLocation}.svelte`;
+    }
+
+    const page = pages[pageUrl];
+
+    if (page == undefined) {
+      throw new Error(`Page not found: ${pageUrl}`);
+    }
+
+    return page
+  },
+
   setup({ el, App, props }) {
     new App({ target: el, props })
   },
+
   progress: {
     color: '#4B5563',
   },
