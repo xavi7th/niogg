5b81d80c38bc99ab2f098ef3284d0c22baf99d45
xavi7th <xavi7th@gmail.com>
2024-04-25 11:45:18 +0100

    Configure ziggy route grouping
    This helps with easier frontend route separation. For example making
    sure admin routes are not available on public pages

---
+++ config/ziggy.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+  // 'whitelist' => ['home', 'api.*'],
+  /**
+   * !Blacklist hidden routes from the general @routes call
+   * ? Access the blacklisted routes using group routes
+   *
+   * * @routes('admin'), @routes(['admin', 'superadmin]), @routes('*')
+   */
+  'blacklist' => ['debugbar.*', 'horizon.*', 'ignition.*'],
+  'groups' => [
+    'public' => [
+      'app.*',
+    ],
+    'auth' => [
+      'auth.login',
+      'auth.logout',
+      'auth.password.*',
+      'auth.register',
+      'auth.verification.notice',
+    ],
+  ],
+];
