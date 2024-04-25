65a6ef3aad91ae4548fd613da788a2e9a62a0c6d
xavi7th <xavi7th@gmail.com>
2024-04-25 14:12:31 +0100

    Configure error handling for Inertia

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,9 @@
 namespace App\Exceptions;
 
 use Throwable;
+use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -18,12 +21,32 @@ class Handler extends ExceptionHandler
     'password_confirmation',
   ];
 
-  /**
-   * Register the exception handling callbacks for the application.
-   */
   public function register(): void
   {
     $this->reportable(function (Throwable $e): void {
     });
+  }
+
+  /**
+   * Render an exception into an HTTP response.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   *
+   * @throws Throwable
+   */
+  public function render($request, Throwable $exception): Response|RedirectResponse|JsonResponse
+  {
+    /** @var Response */
+    $response = parent::render($request, $exception);
+
+    if (in_array($response->status(), [500, 503, 404, 403, 429]) && $request->header('X-Inertia')) {
+      return back()->withFlash(['error' => $exception->getMessage()]);
+    }
+
+    if (in_array($response->status(), [419]) && $request->header('X-Inertia')) {
+      return back()->withFlash(['error' => 'Your session has expired. Please try again']);
+    }
+
+    return $response;
   }
 }
