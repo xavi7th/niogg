0b211ae9431f7445985f4cff5076a2bdfb14c1f2
xavi7th <xavi7th@gmail.com>
2024-05-01 09:36:45 +0100

    Imporve components props handling

--- resources/js/Components/InputLabel.svelte
+++ resources/js/Components/InputLabel.svelte
@@ -1,12 +1,12 @@
 <script>
-  export let label;
+  export let label, className;
 
-  $: props = (({label, ...rest}) => rest)($$props);
+  $: props = (({label, className, ...rest}) => rest)($$props);
 </script>
 
 <template>
   <!-- svelte-ignore a11y-label-has-associated-control -->
-  <label class="block font-medium text-sm text-gray-700 dark:text-gray-300" {...props}>
+  <label class="block font-medium text-sm text-gray-700 dark:text-gray-300 {className}" {...props}>
     {#if label}
       <span>{ label }</span>
     {:else}
