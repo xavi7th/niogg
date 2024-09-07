cf406979be764595541aa813903730b31f1eba50
Akhile E. Daniel <ehi.akhile@gmail.com>
2024-09-07 15:27:13 +0100

    Feat; Launch conference page

---
+++ Modules/Conference/vite.config.js
@@ -0,0 +1,11 @@
+export const paths = [];
+
+export const aliases = {
+  '@conference-pages': '/Modules/Conference/resources/js/Pages',
+  '@conference-shared': '/Modules/Conference/resources/js/Shared',
+  '@conference-components': '/Modules/Conference/resources/js/Components',
+  '@conference-partials': '/Modules/Conference/resources/js/Pages/Partials',
+  '@conference-assets': '/Modules/Conference/resources'
+};
+
+export const concatFiles = [];
