01d03b687f57038d71d5c0b3407e526bfdb8edb4
Akhile E. Daniel <ehi.akhile@gmail.com>
2024-09-10 11:13:07 +0100

    Temp disable auto conference registration

--- Modules/Conference/resources/js/Pages/Partials/LaunchConferenceRegistrationForm.svelte
+++ Modules/Conference/resources/js/Pages/Partials/LaunchConferenceRegistrationForm.svelte
@@ -87,7 +87,7 @@
                 <Dialog.Trigger class="btn btn__secondary" disabled={!details.email || !details.amount}><span>Register Now</span></Dialog.Trigger>
                 <Dialog.Content class="max-w-3xl">
                   <Dialog.Header>
-                    <Dialog.Title class="text-4xl">Choose your Reservation type</Dialog.Title>
+                    <!-- <Dialog.Title class="text-4xl">Choose your Reservation type</Dialog.Title> -->
                     <Dialog.Description>
                       <div class="relative isolate px-6">
                         <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
@@ -151,7 +151,7 @@
                           </div>
                         </div>
 
-                        <Separator class="my-8 bg-slate-200 h-0.5" />
+                        <!-- <Separator class="my-8 bg-slate-200 h-0.5" />
 
                         <div class="mx-auto z-30 pb-6">
                           <div class="text-center">
@@ -167,7 +167,7 @@
                               </button>
                             </div>
                           </div>
-                        </div>
+                        </div> -->
 
                         <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
                           <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#a18802] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
