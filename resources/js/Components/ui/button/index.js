e234450f4a05e9499b51a8a21f7b5e3fa70a425e
Akhile E. Daniel <ehi.akhile@gmail.com>
2024-09-23 16:06:09 +0100

    Update deps

--- resources/js/Components/ui/button/index.js
+++ resources/js/Components/ui/button/index.js
@@ -14,6 +14,7 @@ const buttonVariants = tv( {
     },
     size: {
       default: "h-10 px-4 py-2",
+      xs: "h-6 rounded-sm px-2 py-1 text-xs",
       sm: "h-9 rounded-md px-3",
       lg: "h-11 rounded-md px-8",
       icon: "h-10 w-10",
